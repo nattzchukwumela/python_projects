0cce1c629b66154b561ac8f1266c8e1cdd82c906
nattzchukwumela <chukwumelapraise@gmail.com>
2024-10-21 17:05:33 +0100

    just added buttons to the generate qr class

--- QR-Code-Generator/main.py
+++ QR-Code-Generator/main.py
@@ -1,4 +1,5 @@
 import qrcode
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QWidget, QPushButton, QDialog, QHBoxLayout, QVBoxLayout, QLineEdit
 
 
@@ -13,6 +14,15 @@ class gen_qr(QWidget):
         self.resize(400, 250)
 
         #Add App Widgets
+        #Buttons Widgets
+        font = QFont('../font/poppin/Poppins-medium.ttf', 20)
+        self.text_box = QLineEdit()
+        self.text_box.setFont(font)
+
+        self.reset = QPushButton('reset')
+        self.backspace = QPushButton('backspace')
+
+        #App widgets/layouts
 
 
 

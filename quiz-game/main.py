19ad64619087c6df2f710ae6fa26e0bc59ce513e
nattzchukwumela <chukwumelapraise@gmail.com>
2024-10-26 06:17:51 +0100

    start button now attached function, disabled the skip and end quiz button initially

--- quiz-game/main.py
+++ quiz-game/main.py
@@ -6,10 +6,20 @@ from modules import quiz_questions
 class Quiz(QWidget):
     def __init__(self):
         super().__init__()
-        self.question = None
-        self.answer = None
-        self.difficulty = None
+        self.btn_layout = None
+        self.answers_layout = None
+        self.main_layout = None
+        self.score_label = None
+        self.question_box = None
+        self.end_btn = None
+        self.skip_btn = None
+        self.start_btn = None
+        self.current_question_index = 0
+        self.score = 0
+        self.answer_buttons = []
+        self.setup_ui()
 
+    def setup_ui(self):
         # App setting
         self.setWindowTitle('Quiz Game')
         self.resize(350, 300)
@@ -17,28 +27,53 @@ class Quiz(QWidget):
         # App widgets
         # App text
         font = QFont('../font/poppin/Poppins-medium.ttf', 18) # default app font
-        self.question_box = QLabel('start Question')
+        self.question_box = QLabel('click start to begin')
         self.question_box.setFont(font)
         self.question_box.setAlignment(Qt.AlignCenter)
 
-        # App buttons
-        self.start = QPushButton('start')
-        self.skip = QPushButton('skip')
-        self.end = QPushButton('end quiz')
+        #track score
+        self.score_label = QLabel(f"score {self.score}")
+        self.score_label.setAlignment(Qt.AlignCenter)
 
+        # App buttons
+        self.start_btn = QPushButton('start')
+        self.skip_btn = QPushButton('skip')
+        self.end_btn = QPushButton('end quiz')
+
+        # disable button
+        self.skip_btn.setEnabled(False)
+        self.end_btn.setEnabled(False)
+
+        # connect functions btn
+        self.start_btn.clicked.connect(self.start_quiz)
         # App layouts
-        main_layout = QVBoxLayout()
-        btn_layout = QHBoxLayout()
+        self.main_layout = QVBoxLayout()
+        self.answers_layout = QVBoxLayout()
+        self.btn_layout = QHBoxLayout()
 
         # combine layouts
-        btn_layout.addWidget(self.start)
-        btn_layout.addWidget(self.skip)
-        btn_layout.addWidget(self.end)
+        self.btn_layout.addWidget(self.start_btn)
+        self.btn_layout.addWidget(self.skip_btn)
+        self.btn_layout.addWidget(self.end_btn)
 
         # create layout
-        main_layout.addWidget(self.question_box)
-        main_layout.addLayout(btn_layout)
-        self.setLayout(main_layout)
+        self.main_layout.addWidget(self.score_label)
+        self.main_layout.addWidget(self.question_box)
+        self.main_layout.addLayout(self.answers_layout)
+        self.main_layout.addLayout(self.btn_layout)
+
+        self.setLayout(self.main_layout)
+
+    def start_quiz(self):
+        self.score = 0
+        self.current_question_index = 0
+        self.score_label.setText(f"score {self.score}")
+        self.start_btn.setEnabled(False)
+        self.end_btn.setEnabled(True)
+        self.skip_btn.setEnabled(True)
+        # self.show_question()
+
+
 
 # start/ init app
 if __name__ in '__main__':
